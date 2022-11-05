1af6d812bb51e0f682646cdc154cdfacced41b15
lucasnevespereira <pereiraneveslucas@gmail.com>
2022-11-05 18:14:44 +0100

    init lexer

---
+++ lexer/lexer.go
@@ -0,0 +1,63 @@
+package lexer
+
+import "interpreter/token"
+
+type Lexer struct {
+	input        string
+	position     int  // current position in input (points to current char)
+	readPosition int  // current reading position in input (after current char)
+	ch           byte // current char under examination
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	// init the char + positions
+	l.readChar()
+	return l
+}
+
+// advances our position and get the next char
+// only supports ASCII
+func (l *Lexer) readChar() {
+	// if pos out of range, reset
+	if l.readPosition >= len(l.input) {
+		// ASCII code for 'NULL'
+		l.ch = 0
+	} else {
+		// set char to value of next position
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition += 1
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	}
+	l.readChar()
+	return tok
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
+}
